21b3f0bd68a824413339708a2e1f8d28c601e027
Fernando Di Masi <wildiney@gmail.com>
2018-12-17 10:43:11 -0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+use SendEmail\Hostgator;
+
+require 'Classes/SendEmail.class.php';
+
+$search = array('./','../','/');
+$file = (isset($_REQUEST['file']))? $_REQUEST['file']:null;
+$file = str_replace($search, '', $file);
+
+$message = "<table cellpadding='0' cellspacing='5' border='0'>";
+$message.= "<tr><th style='text-align:right'>FILE</th><td>"                 . $file                                     . "</td>";
+$message.= "<tr><th style='text-align:right'>TIME</th><td>"                 . date("Y-m-d H:i:s")                       . "</td>";
+$message.= "<tr><th style='text-align:right'>REFERER</th><td>"              . @$_SERVER['HTTP_REFERER']                 . "</td>";
+$message.= "<tr><th style='text-align:right'>HTTP_X_FORWARDED_FOR</th><td>" . @$_SERVER['HTTP_X_FORWARDED_FOR']         . "</td>";
+$message.= "<tr><th style='text-align:right'>USER AGENT</th><td>"           . @$_SERVER['HTTP_USER_AGENT']              . "</td>";
+$message.= "<tr><th style='text-align:right'>IP CLIENT</th><td>"            . @$_SERVER['HTTP_CLIENT_IP']               . "</td>";
+$message.= "<tr><th style='text-align:right'>IP ADDRESS</th><td>"           . @$_SERVER['REMOTE_ADDR']                  . "</td>";
+$message.= "<tr><th style='text-align:right'>IP VALUE</th><td>"             . gethostbyaddr($_SERVER['REMOTE_ADDR'])    . "</td>";
+$message.= "</table>";
+
+$email = new Hostgator;
+$email->from('contato@slicedpixel.com');
+$email->to('wildiney@gmail.com');
+
+if (!is_null($file) && $file!='' && $file!="index.php") {
+    if (is_file("./" . $file)) {
+        $email->subject("[Indra] File downloaded ");
+        $email->message($message);
+        $email->send();
+        
+        header('Content-Type: application/octet-stream');
+        header('Content-Transfer-Encoding: Binary');
+        header('Content-disposition: attachment; filename="' . basename($file) . '"');
+        readfile($file);
+    } else {
+        $email->subject("[Indra] Attempt to get a file");
+        $email->message($message);
+        $email->send();
+        die("Erro 404 - File not found");
+    }
+} else {
+    die("Access not allowed");
+}
