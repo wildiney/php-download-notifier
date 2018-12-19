0394d62743058b045c4cf0c7355a777530ab2fb3
Fernando Di Masi <wildiney@gmail.com>
2018-12-19 17:06:42 -0200

    Refatoração

--- index.php
+++ index.php
@@ -9,24 +9,28 @@ $search = array('./','../','/');
 $file = (isset($_REQUEST['file']))? $_REQUEST['file']:null;
 $file = str_replace($search, '', $file);
 
+$email_from='';
+$email_to='';
+$email_subject='';
+
 $message = "<table cellpadding='0' cellspacing='5' border='0'>";
-$message.= "<tr><th style='text-align:right'>FILE</th><td>"                 . $file                                     . "</td>";
-$message.= "<tr><th style='text-align:right'>TIME</th><td>"                 . date("Y-m-d H:i:s")                       . "</td>";
-$message.= "<tr><th style='text-align:right'>REFERER</th><td>"              . @$_SERVER['HTTP_REFERER']                 . "</td>";
-$message.= "<tr><th style='text-align:right'>HTTP_X_FORWARDED_FOR</th><td>" . @$_SERVER['HTTP_X_FORWARDED_FOR']         . "</td>";
-$message.= "<tr><th style='text-align:right'>USER AGENT</th><td>"           . @$_SERVER['HTTP_USER_AGENT']              . "</td>";
-$message.= "<tr><th style='text-align:right'>IP CLIENT</th><td>"            . @$_SERVER['HTTP_CLIENT_IP']               . "</td>";
-$message.= "<tr><th style='text-align:right'>IP ADDRESS</th><td>"           . @$_SERVER['REMOTE_ADDR']                  . "</td>";
-$message.= "<tr><th style='text-align:right'>IP VALUE</th><td>"             . gethostbyaddr($_SERVER['REMOTE_ADDR'])    . "</td>";
+$message.= "    <tr><th style='text-align:right'>FILE</th><td>"                 . $file                                     . "</td>";
+$message.= "    <tr><th style='text-align:right'>TIME</th><td>"                 . date("Y-m-d H:i:s")                       . "</td>";
+$message.= "    <tr><th style='text-align:right'>REFERER</th><td>"              . @$_SERVER['HTTP_REFERER']                 . "</td>";
+$message.= "    <tr><th style='text-align:right'>HTTP_X_FORWARDED_FOR</th><td>" . @$_SERVER['HTTP_X_FORWARDED_FOR']         . "</td>";
+$message.= "    <tr><th style='text-align:right'>USER AGENT</th><td>"           . @$_SERVER['HTTP_USER_AGENT']              . "</td>";
+$message.= "    <tr><th style='text-align:right'>IP CLIENT</th><td>"            . @$_SERVER['HTTP_CLIENT_IP']               . "</td>";
+$message.= "    <tr><th style='text-align:right'>IP ADDRESS</th><td>"           . @$_SERVER['REMOTE_ADDR']                  . "</td>";
+$message.= "    <tr><th style='text-align:right'>IP VALUE</th><td>"             . gethostbyaddr($_SERVER['REMOTE_ADDR'])    . "</td>";
 $message.= "</table>";
 
 $email = new Hostgator;
-$email->from('contato@slicedpixel.com');
-$email->to('wildiney@gmail.com');
+$email->from($email_from);
+$email->to($email_to);
 
 if (!is_null($file) && $file!='' && $file!="index.php") {
     if (is_file("./" . $file)) {
-        $email->subject("[Indra] File downloaded ");
+        $email->subject($email_subject);
         $email->message($message);
         $email->send();
         
@@ -35,7 +39,7 @@ if (!is_null($file) && $file!='' && $file!="index.php") {
         header('Content-disposition: attachment; filename="' . basename($file) . '"');
         readfile($file);
     } else {
-        $email->subject("[Indra] Attempt to get a file");
+        $email->subject($email_subject);
         $email->message($message);
         $email->send();
         die("Erro 404 - File not found");
